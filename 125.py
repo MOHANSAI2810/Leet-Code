75313d105cf4e0f084a8ef7f33981762d2187a3b
pandu <pushadapumohansai@gmail.com>
2025-06-13 21:07:06 +0530

    125

--- 125.py
+++ 125.py
@@ -2,8 +2,8 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         a = []
         for i in s:
-            if i.isalnum():  # only consider alphanumeric characters
-                a.append(i.lower())  # convert to lowercase
+            if i.isalnum():  
+                a.append(i.lower())  
         if a==a[::-1]:
             return True
         else:
