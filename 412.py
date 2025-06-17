e3835856dea78a10c0af7f6c87760f6f7ff03203
pandu <pushadapumohansai@gmail.com>
2025-06-17 21:31:00 +0530

    412

---
+++ 412.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        a=[]
+        for i in range(n):
+            if (i+1)%3==0 and (i+1)%5==0:
+                a.append("FizzBuzz")
+            elif (i+1)%5==0:
+                a.append("Buzz")
+            elif (i+1)%3==0:
+                a.append("Fizz")
+            else:
+                a.append(str(i+1))
+        return a
